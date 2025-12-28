7a2caf86598386bd053b6323488c6d7c27ba7458
MdWarishh <171806296+MdWarishh@users.noreply.github.com>
2025-12-29 02:40:25 +0530

    latest update

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 'use client'
 import Footer from '@/components/footer/Footer'
 import Navbar from '@/components/navbar/Navbar'
+import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState } from 'react'
 
@@ -15,6 +16,12 @@ const page = () => {
       <Link href='/service/service1' className='m-2.5 text-2xl'>Service 1</Link>
       <Link href='/service/service2' className='m-2.5 text-2xl'>Service 2</Link>
       <Link href='/service/service2/service2.1' className='m-2.5 text-2xl'>Service 2.1</Link>
+
+    <img 
+        src="https://images.pexels.com/photos/1933239/pexels-photo-1933239.jpeg"
+        alt="image"
+        style={{ width: "500px", height: "300px" }}
+      />
     </div>
   )
 }
